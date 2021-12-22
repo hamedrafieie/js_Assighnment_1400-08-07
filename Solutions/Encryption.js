0f26f0e5695b3c49aaa03aaed74fb68369e99b07
hamedrafiei <hamed.rafie78@gmail.com>
2021-12-22 21:33:15 +0330

    monir changes

--- Solutions/Encryption.js
+++ Solutions/Encryption.js
@@ -1,7 +1,9 @@
 function Encrypt(str,k) {
        let swapped = str[str.length-1] + str.substr(0,str.length - 1)
+
        function letterChange (swapped){
               return swapped.replace(/[a-zA-Z]/g,function(x){
+                     if (x==='z'){return 'a'}
                      return String.fromCharCode(x.charCodeAt(0)+1)
               })}
        let ans = letterChange(swapped)
@@ -9,7 +11,8 @@ function Encrypt(str,k) {
        console.log(ans)
        if (k > 0) Encrypt(ans,k)
 }
-
+Encrypt("abz",1)
+//abc
 Encrypt("abcd",5)
 // ebcd
 // efcd
